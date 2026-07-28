5918ecb4edc2495027c5ea5bb8a2c3b89914d439
xiaoshangfei <fei523688393@hotmail.com>
2026-07-29 01:19:10 +0800

    feat: add FFMPEG_PATH to ecosystem configuration for video processing

--- apps/web-admin/ecosystem.config.cjs
+++ apps/web-admin/ecosystem.config.cjs
@@ -16,7 +16,8 @@ module.exports = {
         NITRO_PORT: 5000,
         HOST: '127.0.0.1',
         NITRO_HOST: '127.0.0.1',
-        NUXT_BACKEND_BASE_URL: 'http://127.0.0.1:8199/api/'
+        NUXT_BACKEND_BASE_URL: 'http://127.0.0.1:8199/api/',
+        FFMPEG_PATH: '/usr/bin/ffmpeg'
       }
     }
   ]
